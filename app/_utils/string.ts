59d27330dfd0d9b01684a3445bd1c9af25450070
projectcarabaoph <projectcarabao@gmail.com>
2025-07-01 14:06:50 +0800

    feat: utils strings

---
+++ app/_utils/string.ts
@@ -0,0 +1,19 @@
+export const strings = {
+    title: "Sample RSVP",
+    description: "We're excited to celebrate our special sample day with you! Please let us know if you can make it.",
+    eventDate: "2024-06-15", // YYYY-MM-DD format
+    eventDateLabel: "Event Date",
+    eventLocation: "Centra Park, new York, NY 10022",
+    eventLocationLabel: "Event Location",
+    nameLabel: "Full Name",
+    emailLabel: "Email Address",
+    accompanyLabel: "Number of Guests",
+    rsvpLabel: "Will you attend?",
+    yesOption: "Yes, I'll be there!",
+    noOption: "Sorry, I can't make it",
+    submitButton: "Send RSVP",
+    thankYouMessage: "Thank you for your response!",
+    viewOnMapButton: "View Location on Google Map",
+    sendFromEmail: "onboarding@resend.dev",
+    sendToEmail: process.env.EMAIL_TO
+}
