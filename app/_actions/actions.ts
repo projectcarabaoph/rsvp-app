e51ca138b8cddda9893358b94ef37a76fb81fe33
projectcarabaoph <projectcarabao@gmail.com>
2025-07-02 21:02:18 +0800

    refactor: actions, implement resend in submit rsvp

--- app/_actions/actions.ts
+++ app/_actions/actions.ts
@@ -1,6 +1,9 @@
 'use server'
 
+import { Resend } from 'resend'
 import { serverClient } from "../_utils/supabase/server-client"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const submitRsvp = async (formData: FormData) => {
     const supabase = await serverClient()
@@ -20,6 +23,25 @@ export const submitRsvp = async (formData: FormData) => {
         })
 
     if (error) throw new Error(error.message)
+
+    try {
+        await resend.emails.send({
+            from: "RSVP <onboarding@resend.dev>",
+            to: `${email}`,
+            subject: "New RSVP Submission",
+            html: `
+        <h1>New RSVP Submission</h1>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Number of Guests:</strong> ${accompany}</p>
+        <p><strong>Attendance:</strong> ${attendance}</p>
+      `
+        })
+
+
+    } catch (error) {
+        if (error instanceof Error) throw new Error(error.message)
+    }
 
     return data
 }
